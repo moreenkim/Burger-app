4b08d6cbcf1e5dc749320de0e92d0f64e99315c3
Moreen <moreenkim36@gmail.com>
2020-08-17 15:03:49 +0300

    Implement the burger app

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import Aux from '../../hoc/Aux';
+import classes from './Layout.css';
 
 const layout = (props) => (
   <Aux>
     <div>toolbar, sidedrawer, backdrop</div>
-    <main>{props.children}</main>
+    <main className={classes.content}>{props.children}</main>
   </Aux>
 );
 
